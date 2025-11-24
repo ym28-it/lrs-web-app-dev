67c742bdc7e30fd80d0019b354f8968cb238a540
ym28-it <120317084+ym28-it@users.noreply.github.com>
2025-11-24 10:22:36 +0900

    use invoke_run modules

--- experiment/autoexe.sh
+++ experiment/autoexe.sh
@@ -1,11 +1,11 @@
-#!bin/bash
+#!/bin/bash
 for i in *.ine; do
-    time- -p lrs $i /dev/null
+    time -p lrs "$i" /dev/null
 done
 
 
-for i in testfiles/ ; do
-    for s in prog/; do
-        time -p $s $i /dev/null # deviceの略　出力の保存
+for i in testfiles/*.ine ; do
+    for s in prog/*; do
+        time -p "$s" "$i" /dev/null # deviceの略　出力の保存
     done
 done
